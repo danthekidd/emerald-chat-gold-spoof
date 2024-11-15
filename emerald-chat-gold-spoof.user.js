661f19850611e6cb79560dc0f07082506d1fe27e
Dan <133232755+danthekidd@users.noreply.github.com>
2024-11-14 23:14:14 -0500

    wildcard match url

--- emerald-chat-gold-spoof.user.js
+++ emerald-chat-gold-spoof.user.js
@@ -4,8 +4,8 @@
 // @version      1.0.0
 // @description  Spoofs gold on Emerald Chat
 // @author       danthekidd
-// @match        https://emeraldchat.com/app
-// @match        https://beta.emeraldchat.com/app
+// @match        https://emeraldchat.com/app*
+// @match        https://beta.emeraldchat.com/app*
 // @grant        none
 // @icon         https://emeraldchat.com/favicon.ico
 // ==/UserScript==
